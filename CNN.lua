3dfb31d69763010de2679bd92361a6e05704df20
Tim Nieradzik <tim@metastack.pl>
2016-04-03 18:48:57 +0200

    CNN: Refactor code

--- CNN.lua
+++ CNN.lua
@@ -3,131 +3,79 @@ local fun = require "fun"
 
 local CNN = {}
 
-function CNN.convolutionModule(inputFrameSize, outputFrameSize, kernelWidth, backend)
+-- Module consisting of a temporal and a 1-max convolution
+function CNN.convolutionModule(inputFrameSize, outputFrameSize, kernelWidth, strideSize, backend)
   local conv = nn.Sequential()
   if backend == "cl" then
-    conv:add(nn.TemporalConvolution2(inputFrameSize, outputFrameSize, kernelWidth))
+    conv:add(nn.TemporalConvolution2(inputFrameSize, outputFrameSize, kernelWidth, strideSize))
     conv:add(nn.SpatialMaxPooling(outputFrameSize, 1))
   else
-    conv:add(nn.TemporalConvolution(inputFrameSize, outputFrameSize, kernelWidth))
+    conv:add(nn.TemporalConvolution(inputFrameSize, outputFrameSize, kernelWidth, strideSize))
     conv:add(nn.TemporalMaxPooling(outputFrameSize))
   end
   return conv
 end
 
-function CNN.convolutionTable(convType)
-  if convType == "wide" then
-    return nn.ParallelTable()
-  elseif convType == "narrow" then
-    return nn.ConcatTable()
-  end
-end
+-- FIXME: what the value for outputFrameSize should be is unclear for me
+local outputFrameSize = 1
+local strideSize      = 1
 
--- TODO Create separate functions: wideConvolution, narrowConvolution
-function CNN.parallelConvolution(convType, alphabetLen, charEmbeddingLen, inputSize, filterMinWidth, filterMaxWidth, backend)
-  -- FIXME: what the value for outputFrameSize shoud be is unclear for me
-  local outputFrameSize = 1
-
-  local convModule = CNN.convolutionTable(convType)
-  fun.each(
-    function(width)
-      if convType == "wide" then
-        convModule:add(CNN.convolutionModule(inputSize + (width - 1) * 2, outputFrameSize, width, backend))
-      elseif convType == "narrow" then
-        convModule:add(CNN.convolutionModule(charEmbeddingLen, outputFrameSize, width, backend))
-      end
-    end,
-    fun.range(filterMinWidth, filterMaxWidth)
-  )
+-- TODO Support regions (multiple filters with the same width)
+-- http://www.wildml.com/2015/11/understanding-convolutional-neural-networks-for-nlp/
+function CNN.narrowConvolution(inputSize, alphabetLen, charEmbeddingLen, filterMinWidth, filterMaxWidth, backend)
+  local convModule = nn.ConcatTable()
+  fun.range(filterMinWidth, filterMaxWidth):each(function (filterWidth)
+    convModule:add(CNN.convolutionModule(charEmbeddingLen, outputFrameSize, filterWidth, strideSize, backend))
+  end)
 
   local joinModule = nn.Sequential()
-
-  if convType == "wide" then
-    joinModule:add(nn.JoinTable(1))
-    -- FIXME: wrong calculations here
-    local convOutputLength = torch.range(filterMinWidth, filterMaxWidth):sum()
-    joinModule:add(nn.Reshape(convOutputLength))
-
-  elseif convType == "narrow" then
-    joinModule:add(nn.JoinTable(2))
-    local convOutputLength = torch.range(inputSize - (filterMaxWidth - filterMinWidth), inputSize):sum()
-    joinModule:add(nn.Reshape(convOutputLength))
-  end
+  joinModule:add(nn.JoinTable(2))
+  local convOutputLength = torch.range(inputSize - (filterMaxWidth - filterMinWidth), inputSize):sum()
+  joinModule:add(nn.Reshape(convOutputLength))
 
   local net = nn.Sequential()
-  if convType == "narrow" then
-    -- character embedding
-    net:add(nn.Sequencer(nn.LookupTable(alphabetLen, charEmbeddingLen)))
-  elseif convType == "wide" then
-    -- TODO: embedding for wide convolution.
-  end
+  -- TODO Move the embedding layer out of this function?
+  net:add(nn.Sequencer(nn.LookupTable(alphabetLen, charEmbeddingLen)))  -- Character embedding
+  net:add(nn.Sequencer(convModule))  -- Convolution
+  net:add(nn.Sequencer(joinModule))  -- Concatenate results into one vector
 
-  -- convolution itself
-  net:add(nn.Sequencer(convModule))
-  -- concatenate results in one vector
-  net:add(nn.Sequencer(joinModule))
   return net
 end
 
+-- Wide convolutions honour borders by adding zero-padding
+function CNN.wideConvolution(inputSize, filterMinWidth, filterMaxWidth, backend)
+  local convModule = nn.ParallelTable()
+  fun.range(filterMinWidth, filterMaxWidth):each(function (width)
+    convModule:add(CNN.convolutionModule(inputSize + (width - 1) * 2, outputFrameSize, width, backend))
+  end)
+
+  local joinModule = nn.Sequential()
+  joinModule:add(nn.JoinTable(1))
+  -- TODO wrong calculations here
+  local convOutputLength = torch.range(filterMinWidth, filterMaxWidth):sum()
+  joinModule:add(nn.Reshape(convOutputLength))
+
+  local net = nn.Sequential()
+  -- TODO Add embedding layer for wide convolution
+  net:add(nn.Sequencer(convModule))
+  net:add(nn.Sequencer(joinModule))
+
+  return net
+end
+
+-- Input must be padded when using wide convolutions
 function CNN.addPadding(xTable, filterMinWidth, filterMaxWidth)
-  return fun.map(
-  function (tensor)
-    return fun.map(
-      function (width)
-        if width > 1 then
-          local zeros = torch.zeros(tensor:size(1), width - 1)
-          local res = torch.cat(zeros, tensor, 2)
-          return torch.cat(res, zeros, 2)
-        else
-          return tensor
-        end
-      end,
-      fun.range(filterMinWidth, filterMaxWidth)
-    ):totable()
-  end, xTable):totable()
-end
-
-function CNN.prepareDataForConvolution(dataset, convType, filterMinWidth, filterMaxWidth)
-  if convType == "narrow" then
-    return dataset
-  elseif convType == "wide" then
-    return CNN.addPadding(dataset, filterMinWidth, filterMaxWidth)
-  end
-end
-
-function CNN.test()
-  -- CNN hyperparameters
-  local batchSize = 15
-  local filterMinWidth = 1
-  local filterMaxWidth = 10
-  local inputSize = 17
-  local alphabetLen = 25
-  local charEmbeddingLen = 5
-  local convolutionType = "narrow"
---  local convolutionType = "wide"
-
-  -- Dummy data similar to our real dataset.
-  local t = torch.Tensor(batchSize, inputSize)
-  local dataset = {
-    t:random(1, alphabetLen):clone(),
-    t:random(1, alphabetLen):clone(),
-    t:random(1, alphabetLen):clone(),
-    t:random(1, alphabetLen):clone()
-  }
-  print(dataset)
-
-  local input = CNN.prepareDataForConvolution(dataset, convolutionType, filterMinWidth, filterMaxWidth)
-  print("input", input)
-
-  local net = CNN.parallelConvolution(convolutionType, alphabetLen, charEmbeddingLen, inputSize, filterMinWidth, filterMaxWidth)
-  local result = net:forward(input)
-  print("result", result)
-
-  -- checking for number of batch elements.
-  print("Number of elements in batch. Test passed:", assert(result[3]:size(1) == 15))
-
-  -- checking for output features number.
-  print("Output features number. Test passed:", assert(result[4]:size(2) == 125))
+  return fun.iter(xTable):map(function (tensor)
+    return fun.range(filterMinWidth, filterMaxWidth):map(function (width)
+      if width == 1 then
+        return tensor
+      else
+        local zeros = torch.zeros(tensor:size(1), width - 1)
+        local res = torch.cat(zeros, tensor, 2)
+        return torch.cat(res, zeros, 2)
+      end
+    end):totable()
+  end):totable()
 end
 
 return CNN
--- tests/CNN.lua
+++ tests/CNN.lua
@@ -2,29 +2,99 @@ local CNN = require "../CNN"
 
 -- TODO Check all backends
 describe("CNN", function()
-  describe("Model construction", function()
-    it("Convolution module", function()
-      local inputFrameSize  = 10  -- Sequence length
-      local filterWidth     = 5
-      local outputFrameSize = 3
-      local module          = CNN.convolutionModule(inputFrameSize, outputFrameSize, filterWidth, "cpu")
+  it("Convolution module", function()
+    local inputFrameSize  = 10  -- Sequence length
+    local filterWidth     = 5
+    local outputFrameSize = 3
+    local strideSize      = 1
+    local module          = CNN.convolutionModule(inputFrameSize,
+      outputFrameSize, filterWidth, strideSize, "cpu")
 
-      local frames  = 15  -- Batch size
-      local classes = 25
-      local tensor = torch.Tensor(frames, inputFrameSize):random(1, classes)
+    local frames  = 15  -- Batch size
+    local classes = 25
+    local tensor = torch.Tensor(frames, inputFrameSize):random(1, classes)
 
-      -- Temporal convolution
-      -- nOutputFrame x outputFrameSize
-      -- nOutputFrame = (nInputFrame - kW) / dW + 1
-      assert.are.same(
-        module.modules[1]:forward(tensor):size():totable(),
-        { (frames - filterWidth) / 1 + 1, outputFrameSize })
+    -- Temporal convolution
+    -- nOutputFrame x outputFrameSize
+    -- nOutputFrame = (nInputFrame - filterWidth) + 1
+    assert.are.same(
+      module.modules[1]:forward(tensor):size():totable(),
+      { frames - filterWidth + 1, outputFrameSize })
 
-      -- Temporal convolution + max pooling
-      local output = module:forward(tensor)
-      assert.are.same(
-        output:size():totable(),
-        { outputFrameSize, outputFrameSize })
-    end)
+    -- Temporal convolution + max pooling
+    local output = module:forward(tensor)
+    assert.are.same(
+      output:size():totable(),
+      { outputFrameSize, outputFrameSize })
+  end)
+
+  it("Narrow convolution", function()
+    local batchSize   = 15
+    local inputSize   = 17
+    local alphabetLen = 25
+
+    -- Dummy input similar to our real dataset
+    local input = {
+      torch.Tensor(batchSize, inputSize):random(1, alphabetLen),
+      torch.Tensor(batchSize, inputSize):random(1, alphabetLen),
+      torch.Tensor(batchSize, inputSize):random(1, alphabetLen),
+      torch.Tensor(batchSize, inputSize):random(1, alphabetLen)
+    }
+
+    local charEmbeddingLen = 5
+    local filterMinWidth   = 1
+    local filterMaxWidth   = 10
+
+    local net = CNN.narrowConvolution(inputSize, alphabetLen, charEmbeddingLen,
+      filterMinWidth, filterMaxWidth, "cpu")
+    local result = net:forward(input)
+
+    assert.are.same(result[1]:size():totable(), {
+      batchSize,
+      torch.range(inputSize - (filterMaxWidth - filterMinWidth), inputSize):sum()
+    })
+  end)
+
+  it("Padding", function()
+    local batchSize   = 15
+    local inputSize   = 17
+    local alphabetLen = 25
+
+    local input = {
+      torch.Tensor(batchSize, inputSize):random(1, alphabetLen),
+      torch.Tensor(batchSize, inputSize):random(1, alphabetLen),
+      torch.Tensor(batchSize, inputSize):random(1, alphabetLen),
+      torch.Tensor(batchSize, inputSize):random(1, alphabetLen)
+    }
+
+    local filterMinWidth = 1
+    local filterMaxWidth = 10
+
+    -- TODO Test functionality
+    local padded = CNN.addPadding(input, filterMinWidth, filterMaxWidth)
+  end)
+
+  it("Wide convolution", function()
+    local batchSize   = 15
+    local inputSize   = 17
+    local alphabetLen = 25
+
+    -- Dummy input similar to our real dataset
+    local input = {
+      torch.Tensor(batchSize, inputSize):random(1, alphabetLen),
+      torch.Tensor(batchSize, inputSize):random(1, alphabetLen),
+      torch.Tensor(batchSize, inputSize):random(1, alphabetLen),
+      torch.Tensor(batchSize, inputSize):random(1, alphabetLen)
+    }
+
+    local charEmbeddingLen = 5
+    local filterMinWidth   = 1
+    local filterMaxWidth   = 10
+
+    local padded = CNN.addPadding(input, filterMinWidth, filterMaxWidth)
+
+    -- TODO Implement wide convolution
+    -- local net = CNN.wideConvolution(inputSize, filterMinWidth, filterMaxWidth, "cpu")
+    -- local result = net:forward(padded)
   end)
 end)
